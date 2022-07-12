208e92cc1a2a7174aa5b0c5d28c89ac663e96b11
Malcolm Keyes <3.malcolmk@gmail.com>
2022-07-12 10:16:10 -0400

    Done.

--- ruby/recursive_search.rb
+++ ruby/recursive_search.rb
@@ -1,5 +1,13 @@
 def recursive_search(arr, target)
   # type your code in here
+  if (arr.length == 0)
+    return false
+  elsif (arr[0] == target)
+    return true
+  else
+    arr.shift
+    return recursive_search(arr, target)
+  end
 end
 
 if __FILE__ == $PROGRAM_NAME
@@ -16,3 +24,16 @@ end
 
 # Please add your pseudocode to this file
 # And a written explanation of your solution
+
+# problem
+# given an array and a value, return true or false if the value is in the array
+# solve the problem recursively
+
+# psuedocode
+# define a method called recursive_search
+# check if the length of the array is > 0
+  # if so return false
+# check if the target is equal to the first element in the array
+  # if so return true
+# else
+  # return recursive_search of the array minus the first element and the target
